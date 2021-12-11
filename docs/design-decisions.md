dd6018b65022a67746fc8b6dc40801829f41bd99
avimehenwal <avi.mehanwal@gmail.com>
2021-12-11 19:14:06 +0100

    chore(frontend): :recycle: cleanup

---
+++ docs/design-decisions.md
@@ -0,0 +1,6 @@
+# Design Decisions
+
+build using
+
+- best UI Library for react
+- best framework to build react apps
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { config } from '../ions/appData'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{config.title}</title>
+        <meta name="description" content={config.title} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
