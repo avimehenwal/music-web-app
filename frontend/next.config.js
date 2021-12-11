4645445089987b731d9d157fd8bb9cf49bacc617
avimehenwal <avi.mehanwal@gmail.com>
2021-12-11 19:20:41 +0100

    build(frontend): :white_check_mark: add MDX markdown support

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+})
