ae17eb58d6cb66bb156135dee17fa0ddd53ad8af
avimehenwal <avi.mehanwal@gmail.com>
2021-12-12 01:58:05 +0100

    style(frontend): :construction_worker: cosmetic change

--- frontend/component/BasicList.tsx
+++ frontend/component/BasicList.tsx
@@ -1,25 +1,13 @@
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+import { styled, useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import Image from 'next/image';
 import * as React from 'react';
-import { SongDetail } from './SongDetail';
-import { useTheme } from '@mui/material/styles';
-import { styled } from '@mui/material/styles';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import { isBoxedPrimitive } from 'util/types';
-
 
 interface BasicListProps {
   items: any[]
@@ -28,9 +16,9 @@ interface BasicListProps {
 const DataStyle = styled('div')(({ theme }) => ({
   display: 'inline-flex',
   padding: '0.1rem',
-  marginLeft: '1rem'
+  marginLeft: '1rem',
+  // fontSize: '6'
 }));
-
 
 
 export const BasicList: React.FC<BasicListProps> = ({ items }) => {
@@ -48,18 +36,6 @@ export const BasicList: React.FC<BasicListProps> = ({ items }) => {
       }}>
         {items?.map(item => (
           <>
-            {/* <ListItem key={item.id}>
-              <Image
-                className='on-hover-playback-image'
-                src={item.cover_image_path}
-                alt={item.name}
-                width={200}
-                height={200}
-              />
-              <SongDetail data={item} />
-            </ListItem> */}
-
-            {/* <Divider /> */}
             <Card sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
               <Box sx={{ display: 'flex', flexDirection: 'column', margin: '2rem' }}>
                 <CardContent sx={{ flex: '1 0 auto' }}>
@@ -71,17 +47,12 @@ export const BasicList: React.FC<BasicListProps> = ({ items }) => {
                   </Typography>
                 </CardContent>
 
-                {/* <IconButton aria-label="play/pause" color='secondary' sx={{ borderRadius: '0', marginBottom: '0.6em' }}>
-                  <PlayArrowIcon sx={{ height: 50, width: 50 }} />
-                </IconButton> */}
-
-                <Box sx={{ marginBottom: '0.5em' }}>
+                <Box sx={{ marginBottom: '1em' }}>
                   <audio controls>
                     <source src={item.music_file_path} type={item.music_file_mimetype} />
                     Your browser does not support the audio element.
                   </audio>
                 </Box>
-
 
                 <DataStyle>
                   <FavoriteBorderOutlinedIcon color="info" fontSize="small" />
@@ -97,7 +68,7 @@ export const BasicList: React.FC<BasicListProps> = ({ items }) => {
 
               <CardMedia
                 component="img"
-                sx={{ width: 200 }}
+                sx={{ height: 300, width: 300 }}
                 image={item.cover_image_path}
                 alt={item.name}
               />
