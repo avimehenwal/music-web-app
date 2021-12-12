3b46f0c616e7442985c046b087f35d1cdc1991e1
avimehenwal <avi.mehanwal@gmail.com>
2021-12-12 14:47:42 +0100

    refactor(nextjs): :zap: data fetching from server

--- frontend/ions/hooks.ts
+++ frontend/ions/hooks.ts
@@ -4,9 +4,9 @@ import useSWR from 'swr'
 
 const fetcherData = (args: string) => fetch(args).then(res => res.json()).then(output => output.data)
 
-const fetcher = (args: string) => fetch(args).then(res => res.json())
+export const fetcher = (args: string) => fetch(args).then(res => res.json())
 
-const trendingDataUrl: string = `https://api-stg.jam-community.com/song/trending`
+export const trendingDataUrl: string = `https://api-stg.jam-community.com/song/trending`
 
 export const useGetTrendingData = () => {
   const { data, error } = useSWR(trendingDataUrl, fetcher)
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,33 @@
-import * as React from "react";
 import type { NextPage } from 'next';
-import { PageLayout1 } from '../component/PageLayout1'
-import { useGetTrendingData } from '../ions/hooks'
-import { BasicList } from '../component/BasicList'
+import { GetStaticProps } from 'next';
+import * as React from "react";
+import { BasicList } from '../component/BasicList';
+import { PageLayout1 } from '../component/PageLayout1';
+import { fetcher, trendingDataUrl } from '../ions/hooks';
 
-const HomePage: NextPage = () => {
-  const { data, isLoading, error } = useGetTrendingData()
+
+const HomePage: NextPage = ({ data }) => {
 
   return (
     <PageLayout1>
       {data && <BasicList items={data} />}
     </PageLayout1>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  // `getStaticProps` is executed on the server side.
+  const data = await fetcher(trendingDataUrl)
+  console.log('SERVER: fetching data')
+
+  return {
+    props: {
+      data,
+      fallback: {
+        '/api/trending': data
+      }
+    }
+  }
 }
 
 export default HomePage
